d82cfe4bfac4196f00c9d9646a03b6ef08216460
koga yusuke <web@yusukelog.com>
2020-05-29 21:55:13 +0900

    firebase setting update

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@
           dark
           @click="doLgoin"
         >
-          Twitterでログインする
+          Googleでログインする
         </v-btn>
       </v-col>
     </v-row>
@@ -30,31 +30,17 @@
 </template>
 
 <script>
-import firebase from '~/plugins/firebase'
-
 export default {
   data() {
     return {
       image_src: require('~/assets/images/illustrain10-nomimono10.png')
     }
   },
-  created() {
-    this.readData()
-  },
   methods: {
-    async readData() {
-      const db = firebase.firestore()
-      const res = await db.doc('users/test').get()
-      console.log(res.data().name)
-    },
     // ログイン処理
     doLgoin() {
-      const provider = new firebase.auth.TwitterAuthProvider()
-      firebase.auth().signInWithPopup(provider)
-    },
-    // ログアウト処理
-    doLogout() {
-      firebase.auth().signOut()
+      const provider = new this.$firebase.auth.GoogleAuthProvider()
+      this.$fireAuth.signInWithRedirect(provider)
     }
   }
 }
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,18 +1,16 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+import config from './../firebaseConfig.json'
 
-// .env に設定した値を取得してる
-const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  databaseURL: process.env.FB_DATABASE_URL,
-  projectId: process.env.FB_PROJECTID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-  appId: process.env.FB_APP_ID,
-  measurementId: process.env.FB_MEASUREMENT_ID
+if (!firebase.apps.length) {
+  firebase.initializeApp({ ...config })
 }
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-
-export default firebase
+export default ({ app }, inject) => {
+  inject('firebase', firebase)
+  inject('firestore', firebase.firestore())
+  inject('fireAuth', firebase.auth())
+  inject('fireStorage', firebase.storage())
+}
