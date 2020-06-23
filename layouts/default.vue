4b07195375ca38ad9f80e34b6e03c6a1f6fcee30
koga yusuke <koga@pop-web.net>
2020-06-23 22:18:58 +0900

    add register

--- layouts/default.vue
+++ layouts/default.vue
@@ -42,7 +42,7 @@
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
 
-    <v-dialog v-model="dialog">
+    <v-dialog v-model="dialog" max-width="300">
       <CreateThreadModal @closeModal="closeModal" />
     </v-dialog>
   </v-app>
---
+++ pages/register.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="ニックネーム"
+      required
+    ></v-text-field>
+    <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+      登録
+    </v-btn>
+  </v-form>
+</template>
